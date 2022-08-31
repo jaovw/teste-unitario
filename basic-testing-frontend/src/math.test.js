3535cd1ee979721816b1630f0fe9cf53de35ee23
jaovw <joojvw@gmail.com>
2022-08-31 19:26:13 -0300

    Mais duas validacoes de string e string numerica

--- basic-testing-frontend/src/math.test.js
+++ basic-testing-frontend/src/math.test.js
@@ -1,7 +1,7 @@
 import { expect, it } from 'vitest'
 import { add } from './math'
 
-it('somar todos os elementos no array', () => {
+it('Deve somar todos os elementos no array', () => {
     
     const numeros = [4,4]
 
@@ -14,3 +14,24 @@ it('somar todos os elementos no array', () => {
     expect(resultado).toBe(resultadoEsperado)
 })
 
+it('Deve retornar NaN caso seja inserido um valor nao desejado', () => {
+
+    const input = ['numero', 1]
+
+    const resultado = add(input)
+
+    expect(resultado).toBeNaN()
+})
+
+it('Deve resultar na soma de dois arrauys numericos inseridos', () => {
+    
+    const numeros =  ['2','5']
+
+    const resultado = add(numeros)
+
+    const resultadoEsperado = numeros.reduce(
+        (primeiroValor, segundovalor) => +primeiroValor + +segundovalor, 0
+    )
+    
+    expect(resultado).toBe(resultadoEsperado)
+})
