8cf63f775772212c1c4c1f6d421cf1fe143deaa7
jaovw <joojvw@gmail.com>
2022-08-31 20:53:21 -0300

    Multiple expect

--- basic-testing-frontend/src/util/numbers.test.js
+++ basic-testing-frontend/src/util/numbers.test.js
@@ -7,14 +7,18 @@ it('Deve tranformar string numerica em numero', () => {
 
     const resultado = transformToNumber(numero)
 
-    expect(resultado).toBeTypeOf('number')
+    // expect(resultado).toBeTypeOf('number')
+    expect(resultado).toBe(1)
 })
 
 it('Deve retornar NaN para string nao numericas', () => {
 
     const input = 'string'
+    const input2 = {}
 
     const resultado = transformToNumber(input)
+    const resultado2 = transformToNumber(input2)
 
     expect(resultado).toBeNaN()
+    expect(resultado2).toBeNaN()
 })
