7763eacf0280bcedb8c38be7a4f52fce74350a25
jaovw <joojvw@gmail.com>
2022-09-01 15:29:17 -0300

    Teste para nova funcao criada

---
+++ basic-testing-frontend/src/output.test.js
@@ -0,0 +1,19 @@
+import { describe, it, expect } from 'vitest'
+import { generateResultText } from './output.js'
+
+describe('generateResultText()', () => {
+    it('Deve retornar uma string de qualquer forma', () => {
+
+        const valor1 = 1
+        const valor2 = 'ffff'
+        const valor3 = false
+
+        const resultado1 = generateResultText(valor1)
+        const resultado2 = generateResultText(valor2)
+        const resultado3 = generateResultText(valor3)
+
+        expect(resultado1).toBeTypeOf('string')
+        expect(resultado2).toBeTypeOf('string')
+        expect(resultado3).toBeTypeOf('string')
+    })
+})
