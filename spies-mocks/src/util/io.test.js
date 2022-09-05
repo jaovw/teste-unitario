78058277625fde829551a336a456cde08786aa91
jaovw <joojvw@gmail.com>
2022-09-05 16:09:11 -0300

    Mudando o comportamento da mock

--- spies-mocks/src/util/io.test.js
+++ spies-mocks/src/util/io.test.js
@@ -3,6 +3,15 @@ import { promises as fs } from 'fs'
 import writeData from './io'
 
 vi.mock('fs')
+vi.mock('path', () => {
+    return {
+        default: {
+            join: (...args) => {
+                return args[args.length - 1]
+            }
+        }
+    }
+})
 
 it('Deve executar o metodo fs,writeFile()', () => {
 
@@ -11,6 +20,7 @@ it('Deve executar o metodo fs,writeFile()', () => {
 
     writeData(testData, testFileName)
     // return expect(writeData(testData, testFileName)).resolves.toBeUndefined()
+    // expect(fs.writeFile).toBeCalled()
 
-    expect(fs.writeFile).toBeCalled()
+    expect(fs.writeFile).toBeCalledWith(testFileName, testData)
 })
