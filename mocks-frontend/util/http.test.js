afaedbd7d1904459974da68a17f23626bf47e9cf
jaovw <joojvw@gmail.com>
2022-09-06 21:49:03 -0300

    Teste utilizando mock em funcao global

---
+++ mocks-frontend/util/http.test.js
@@ -0,0 +1,34 @@
+import { expect, it, vi } from 'vitest'
+import { sendDataRequest } from './http'
+//  MOKANDO AS REQUISICOES HTTP PARA NAO REALIZA-LAS EM UM CONTEXTO DE TESTE
+//  PODENDO SER EM RELACAO A LIB AXIOS, TERIA QUE UTILIZAR O DIRETORIO __mocks__ E CRIAR A FUNCAO
+const responseData = {key: 'key'}
+//  CRIANDO A MESMA FUNCIONALIDADE DO FETCH
+const newFetch = vi.fn((url, options) => {
+
+    return new Promise((resolve, reject) => {
+
+        const response ={
+            ok: true,
+            json() {
+                return new Promise((resolve, reject) => {
+                    resolve(responseData)
+                })
+            }
+        }
+
+        resolve(response)
+    })
+})
+
+//  METODO PARA MOKAR METODOS/FUNCOES GLOBAIS (FETCH)
+vi.stubGlobal('fetch', newFetch)
+
+it('Deve retornar qualquer response valida', async () => {
+
+    const data = {key: 'someKey'}
+
+    const result = await sendDataRequest(data) 
+
+    expect(result).toEqual(responseData)
+})
